d4c12b02120099716a65c2519944471d7f8a6b0f
zgeniuscoders <zgeniuscoders.com>
2024-05-26 18:50:40 +0100

    Initial Commit

---
+++ my matches/my_matchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_matchesApp.swift
+//  my matches
+//
+//  Created by ZGenius on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct my_matchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
