947c9578a8dd20ca1d802405e6226717863df366
zgeniuscoders <zgeniuscoders.com>
2024-05-27 23:28:49 +0100

    End design profileView

---
+++ my matches/Views/MessageListView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageListView.swift
+//  my matches
+//
+//  Created by ZGenius on 27/05/2024.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageListView()
+}
